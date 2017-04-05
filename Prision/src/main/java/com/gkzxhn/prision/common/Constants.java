0cd5889ff46e88d8ec17a56ea795d3de2fe4fa16
Raleigh.Luo <Raleigh.Luo@live.com>
2017-04-05 15:59:25 +0800

    Splash UI

--- Prision/src/main/java/com/gkzxhn/prision/common/Constants.java
+++ Prision/src/main/java/com/gkzxhn/prision/common/Constants.java
@@ -4,5 +4,9 @@ package com.gkzxhn.prision.common;
  * Created by Raleigh.Luo on 17/3/29.
  */
 
-public class Constants {
+public interface Constants {
+    final String USER_TABLE="user_table";
+    final String USER_IS_UNAUTHORIZED="isUnauthorized";
+    final String USER_ID="user_id";
+    final String IS_FIRST_IN="is_first_in";
 }
